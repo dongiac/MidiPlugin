7b882abce619c24bada80e702f001a91a9d75804
Giacomo Di Giovanni <diggio1996@gmail.com>
2021-05-21 18:23:33 +0200

    Aggiunto Display
    Il display consente di selezionare la porta midi

--- src/MidiMPEModule.cpp
+++ src/MidiMPEModule.cpp
@@ -7,6 +7,7 @@ struct MidiMPEModule : Module {
 	};
 
 	enum InputIds {
+		INPUT1,
 		NUM_INPUTS,
 	};
 
@@ -17,6 +18,8 @@ struct MidiMPEModule : Module {
 	enum LightsIds {
 		NUM_LIGHTS,
 	};
+
+	midi::InputQueue midiInput;
 
 
 	MidiMPEModule() {
@@ -33,16 +36,38 @@ struct MidiMPEModule : Module {
 
 };
 
+
+
 /* MODULE WIDGET */
 struct MidiMPEModuleWidget : ModuleWidget {
 	MidiMPEModuleWidget(MidiMPEModule* module) {
 
 		setModule(module);
-		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/HelloModule.svg")));
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/MidiPlugin.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		//Modulo per scelta ingressi MIDI 
+		MidiWidget* midiWidget = createWidget<MidiWidget>(mm2px(Vec(3.41891, 14.8373)));
+		midiWidget->box.size = mm2px(Vec(33.840, 28));
+		midiWidget->setMidiPort(module ? &module->midiInput : NULL);
+		addChild(midiWidget);
 	}
 
+	
+/*	void appendContextMenu(Menu* menu) override {
+		MidiMPEModule* module = dynamic_cast<MidiMPEModule*>(this->module);
+
+		menu->addChild(new MenuEntry);
+
+	
+	}
+*/
 };
 
 
-Model * modelMidiMPE = createModel<MidiMPEModule, MidiMPEModuleWidget>("HelloWorld");
+Model * modelMidiMPE = createModel<MidiMPEModule, MidiMPEModuleWidget>("MidiMPE");
 
