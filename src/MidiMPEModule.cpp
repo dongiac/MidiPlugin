b1c2407e33d86015fc2e668ec66362d94e1e9308
Giacomo Di Giovanni <diggio1996@gmail.com>
2021-05-29 11:19:06 +0200

    Update MidiMPEModule.cpp
    Funzionamento output

--- src/MidiMPEModule.cpp
+++ src/MidiMPEModule.cpp
@@ -69,13 +69,21 @@ struct MidiMPEModule : Module {
 		for(int channel = 0; channel<16; channel++){
 			//ci sono da 0 a 11 ottave, ogni ottava è 12 "unità", se pongo 0V = C5 (60) allora vado da -5V(C0) a 5V(C11)
 			//qualsiasi è la nota sottraggo 60, centrandomi in C5 e divido per le 12 unità ottenendo 1V per ottava
-			outputs[VOCT].setVoltage((notes[channel] - 60.f) - 12.f, channel);
+			outputs[VOCT].setVoltage((notes[channel] - 60.f) / 12.f, channel);
 			//controlla il gate di ogni canale, se true 10v se false 0V
 			outputs[GATE].setVoltage(gates[channel] ? 10.f : 0.f, channel);
-
+			//output da range 0-127 a 0-10
+			outputs[STRIKE].setVoltage((strike[channel] / 127.f) * 10.f, channel);
+			outputs[LIFT].setVoltage((lift[channel] / 127.f) * 10.f, channel);
+			outputs[PRESS].setVoltage((press[channel] / 127.f) * 10.f, channel);
+			outputs[SLIDE].setVoltage((slide[channel] / 127.f) * 10.f, channel);
+			// output glide tra -5V e 5V
+			outputs[GLIDE].setVoltage(((glide[channel]) * 10.f / 16384.f -5.f), channel);
+			//outputs[GLIDE].setVoltage(glide[channel] / 16384.f, channel);
+			 
 		}
 
-
+	
 
 		while (midiInput.shift(&msg)) {
 			
@@ -86,7 +94,7 @@ struct MidiMPEModule : Module {
 					int channel = msg.getChannel(); 
 					notes[channel] = msg.getNote();
 					gates[channel] = false;	
-						//Prendo qui la Velocity (Lift)
+						//Prendo la Velocity (Lift)
 						lift[channel] = msg.getValue();
 				} break;
 
@@ -95,20 +103,41 @@ struct MidiMPEModule : Module {
 					int channel = msg.getChannel(); 
 					notes[channel] = msg.getNote();
 					gates[channel] = true;					
-						// prendo qui la Velocity (Strike)
+						// prendo la Velocity (Strike)
 						strike[channel] = msg.getValue();
+
 				} break;
 
+				//Essendo MPE, le note sono sui channel, quindi musicalmente non c'è differenza tra Poly e Channel pressure
 				case 0xa: {
+					//Poly Pressure
 					//Aftertouch (Press)
+					int channel = msg.getChannel();
+					press[channel] = msg.getValue();	
+									
+				} break;
+
+				case 0xd: {
+					//Channel Pressure
+					//Aftertouch (Press)
+					int channel = msg.getChannel();
+					press[channel] = msg.getNote();	
+									
 				} break;
 
 				case 0xe: {
 					//Pitch Wheel (Glide)
+					int channel = msg.getChannel();
+					//Ho LSB (00-7F) su Note e MSB (00-7F) su Value, shifto value e sommo note
+					glide[channel] = ((uint16_t) msg.getValue() << 7) | msg.getNote();
 				} break;
 
 				case 0xb: {
 					//CC74 (Slide)
+					int channel = msg.getChannel();
+					if(msg.getNote() == 74){ //la roli manda sul controller 74
+						slide[channel] = msg.getValue();
+					}
 				} break;
 				default: break;
 
